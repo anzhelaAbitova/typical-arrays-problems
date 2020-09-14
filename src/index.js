fa86f443f163f4b1b9ec2b931ebace2d5e0b947b
anzhelaAbitova <55100446+anzhelaAbitova@users.noreply.github.com>
2020-09-14 12:53:37 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+    return (array && array.length !== 0) ? Math.min.apply(null, array) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    return (array && array.length !== 0) ? Math.max.apply(null, array) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array && array.length !== 0) {
+        let sum = 0;
+        array.forEach(item => sum += item);
+        return sum / array.length;
+    }
+    else {
+        return 0;
+    }
 }
